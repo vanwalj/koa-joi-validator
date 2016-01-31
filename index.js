42f1102e8cbafe12a883205dbf28cbb38d9abcb3
Jordan <j.vanwall@gmail.com>
2016-01-31 14:38:26 +0100

    Updated to 2.0

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 'use strict';
 
+const Promise = require('bluebird');
 const Joi = module.exports.Joi = module.exports.joi = require('joi');
 const coBody = require('co-body');
 
-function validateAsync(value, schema, options) {
-    return new Promise(function (resolve, reject) {
-        return Joi.validate(value, schema, options, function (err, val) {
-            if (err) {
-                return reject(err);
-            }
-            console.log('val', val);
-            return resolve(val);
-        });
-    });
-}
+const _validate = Promise.promisify(Joi.validate, { context: Joi });
 
 module.exports.validator = function (opts) {
     const _opts = Object.assign({
         failure: 400
     }, opts);
     return function * (next) {
+        this.request.validated = {};
         if (_opts.type && coBody[_opts.type]) {
             this.request.body = yield coBody[_opts.type](this);
         }
         try {
             if (_opts.body) {
-                this.request.body = yield validateAsync(this.request.body, _opts.body, _opts.options);
+                this.request.validated.body = yield _validate(this.request.body, _opts.body, _opts.options);
+                this.request.body = this.request.validated.body;
             }
             if (_opts.headers) {
-                this.request.headers = yield validateAsync(this.request.headers, _opts.headers, _opts.options);
+                this.request.validated.headers = yield _validate(this.request.headers, _opts.headers, _opts.options);
+                this.request.headers = this.request.validated.headers;
             }
             if (_opts.query) {
-                const tmp = yield validateAsync(this.request.query, _opts.query, _opts.options);
-                console.log('tmp', tmp);
-                this.request.query = tmp;
-                console.log('this.request.query', this.request.query);
+                this.request.validated.query = yield _validate(this.request.query, _opts.query, _opts.options);
+                this.request.query = this.request.validated.query;
             }
             if (_opts.params) {
-                this.request.params = yield validateAsync(this.request.params, _opts.params, _opts.options);
+                this.request.validated.params = yield _validate(this.request.params, _opts.params, _opts.options);
+                this.request.params = this.request.validated.params;
             }
         } catch (e) {
             this.throw(_opts.failure, e);
         }
-        console.log('query', this.request.query);
         yield next;
     };
 };
